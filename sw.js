51075666c1db3e57dc22db21041f175a4c9eb17a
maidhily-s <maidhilymadhavam@gmail.com>
2026-02-14 08:58:03 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "flames-app-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html"
+      ]);
+    })
+  );
+});
